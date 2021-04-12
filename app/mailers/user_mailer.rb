b60deda1a26d14f64962c4ea1715c8e027365fbb
Prashant <prashant.aggarwal@berylsystems.com>
2021-04-12 22:18:32 +0530

    mailer with attechment and crone job task

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,7 @@ class UserMailer < ApplicationMailer
 	def job_post
 	  @email = params[:email]
     @jd = 'This is mail for the possition of ROR developer'
+    attachments['free_book.pdf'] = File.read('/Users/prashant/Desktop/GS-GIT-160221-0611.pdf')
     mail(to: 'prashant.berylsystems@gmail.com', subject: 'Opening for ROR developer')
 	end
 end
