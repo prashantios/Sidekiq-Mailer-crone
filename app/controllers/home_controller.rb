4d5d599fc69577343b65ea6fa23cd5f9ef19aa06
Prashant <prashant.aggarwal@berylsystems.com>
2021-04-13 12:30:36 +0530

    set indentation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     users = UserPosition.new(position: "4").top_user
-  	puts(users[0].name)
-  	Sidekiq::Client.enqueue_to_in("default", Time.now, JobPostWorker)
+    puts(users[0].name)
+    Sidekiq::Client.enqueue_to_in("default", Time.now, JobPostWorker)
   end
 end
