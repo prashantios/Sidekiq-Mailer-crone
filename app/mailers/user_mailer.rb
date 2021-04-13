cf1d64ea724ba89c00ceb376e84ee0acf6d6b895
Prashant <prashant.aggarwal@berylsystems.com>
2021-04-13 12:31:55 +0530

    set indentation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,9 @@
 class UserMailer < ApplicationMailer
 	# default from: 'prashant.berylsystems@gmai.com'
-
-	def job_post
-	  @email = params[:email]
+  def job_post
+    @email = params[:email]
     @jd = 'This is mail for the possition of ROR developer'
     attachments['free_book.pdf'] = File.read('/Users/prashant/Desktop/GS-GIT-160221-0611.pdf')
     mail(to: 'prashant.berylsystems@gmail.com', subject: 'Opening for ROR developer')
-	end
+  end
 end
