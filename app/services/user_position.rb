821c160c7b540506e4f258e1e418780782c97628
Prashant <prashant.aggarwal@berylsystems.com>
2021-04-13 12:23:07 +0530

    set indentation

--- app/services/user_position.rb
+++ app/services/user_position.rb
@@ -1,9 +1,9 @@
 class UserPosition
-  def initialize(params)
-	  @position = params[:position]
+	def initialize(params)
+		@position = params[:position]
 	end
-  
+	
 	def top_user
-	  return User.where("position = #{@position}")
+		return User.where("position = #{@position}")
 	end
 end
