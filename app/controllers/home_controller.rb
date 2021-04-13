0d5921ddf1931126748a99dd00f10aaf902523ce
Prashant <prashant.aggarwal@berylsystems.com>
2021-04-13 12:13:55 +0530

    set intendation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	users = UserPosition.new(position: "4").top_user
+    users = UserPosition.new(position: "4").top_user
   	puts(users[0].name)
   	Sidekiq::Client.enqueue_to_in("default", Time.now, JobPostWorker)
   end
--- lib/tasks/user.rake
+++ lib/tasks/user.rake
@@ -3,5 +3,4 @@ namespace :user do
   task create_user: :environment do
   	User.create(name: 'Prashant', position: '5', roll_number: '149')
   end
-
 end
